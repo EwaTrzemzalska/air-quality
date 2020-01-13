8438eab7b4cab4d988a2df66880b241a97c915a7
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-13 15:46:16 +0100

    Adding access key and endpoint

--- src/air_quality/core.clj
+++ src/air_quality/core.clj
@@ -1,7 +1,15 @@
 (ns air-quality.core
   (:gen-class))
 
+
+(def access-key "0bf2bab980a7b6eaf5f8e4f9451e2fc2de54c39b")
+(def endpoint "https://api.waqi.info/feed/")
+
+
+
+
+
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
