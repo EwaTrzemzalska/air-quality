0a2d30583e8f4cf98bab92214bb607de2509c3d8
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-13 16:12:59 +0100

    Adding function to generate request

--- src/air_quality/core.clj
+++ src/air_quality/core.clj
@@ -5,11 +5,9 @@
 (def access-key "0bf2bab980a7b6eaf5f8e4f9451e2fc2de54c39b")
 (def endpoint "https://api.waqi.info/feed/")
 
-
-
-
-
+(defn build-request-about-city-str [city]
+  (str endpoint city "/?token=" access-key))
 
 (defn -main
-  [& args]
-  (println "Hello, World!"))
+  [city]
+  (println (build-request-about-city-str city)))
