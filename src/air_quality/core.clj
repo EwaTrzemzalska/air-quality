84fd35a97e2ea5562997c332316436413d7bc623
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-14 20:19:17 +0100

    Adding function to get air quality

--- src/air_quality/core.clj
+++ src/air_quality/core.clj
@@ -15,9 +15,48 @@
       :body
       (cheshire/parse-string true)))
 
+(defn get-air-quality 
+  "If city found returns a map with air quality informations from waqi API.
 
+   A map in following format is returned:
+   {:status ok
+    :data {:aqi 148
+           :idx 8689
+           :attributions [{:url http://monitoring.krakow.pios.gov.pl/
+                           :name Regional Inspectorate for Environmental Protection in Krakow (WIOŚ - Wojewódzki Inspektorat Ochrony Środowiska w Krakowie)
+                           :logo poland-wios-krakowie.png} 
+                          {:url http://powietrze.gios.gov.pl/
+                           :name Główny inspektorat ochrony środowiska
+                           :logo poland-wios-national.png} 
+                          {:url https://waqi.info/
+                           :name World Air Quality Index Project}]
+    :city {:geo [50.057447 19.946008]
+           :name Kraków-ul. Dietla Małopolska Poland
+           :url https://aqicn.org/city/poland/malopolska/krakow-ul.-dietla}
+           :dominentpol pm25
+           :iaqi {:co {:v 15.2}
+           :no2 {:v 24.2}
+           :p {:v 1019.7}
+           :pm10 {:v 58}
+           :pm25 {:v 148}
+           :t {:v -0.1}
+           :w {:v 0.2}
+           :wg {:v 2.5}}
+           :time {:s 2020-01-14 17:00:00
+           :tz +01:00
+           :v 1579021200}
+           :debug {:sync 2020-01-15T03:49:28+09:00}}}"
+  
+  [city]
+  (let [response (send-request (build-request-about-city-str city))]
+    (if (= "ok" (get response :status))
+      response
+      (println "Please provide correct city")
+      )
+    )
+  )
 
 
 (defn -main
   [city]
-  (println (send-request city)))
+  (println (get-air-quality city)))
