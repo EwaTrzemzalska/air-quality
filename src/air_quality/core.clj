784fff6770edc5ce88150c64ceb2e0dd00afc662
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-17 17:57:21 +0100

    Adding new namespace for UI

--- src/air_quality/core.clj
+++ src/air_quality/core.clj
@@ -1,48 +1,8 @@
 (ns air-quality.core
-  (:require [air-quality.waqi :as waqi])
+  (:require [air-quality.UI :as ui])
   (:gen-class))
 
-(defn air-quality-info->location [air-quality-info]
-  (get-in air-quality-info [:data :city :name]))
-
-(defn air-quality-info->aqi [air-quality-info]
-  (get-in air-quality-info [:data :aqi]))
-
-(defn air-quality-info->pm25 [air-quality-info]
-  (get-in air-quality-info [:data :iaqi :pm25 :v]))
-
-(defn air-quality-info->pm10 [air-quality-info]
-  (get-in air-quality-info [:data :iaqi :pm10 :v]))
-
-(defn location-str [air-quality-info]
-  (let [location (air-quality-info->location air-quality-info)]
-    (when location
-      (str "In " location))))
-
-(defn aqi-str [air-quality-info]
-  (let [aqi (air-quality-info->aqi air-quality-info)]
-    (if aqi
-      (str " - AQI: " aqi " (" (* 100 (/ aqi 50)) "%)")
-      "no data about AQI, ")))
-
-(defn pm25-str [air-quality-info]
-  (let [pm25 (air-quality-info->pm25 air-quality-info)]
-    (if pm25
-     (str ", PM 2.5: " pm25 " μg/m3 (" (* 100 (/ pm25 50)) "%)")
-      ", no data about PM 2.5 in this location")))
-
-(defn pm10-str [air-quality-info]
-  (let [pm10 (air-quality-info->pm10 air-quality-info)]
-    (if pm10
-     (str ", PM 10: " pm10 " μg/m3 (" (* 100 (/ pm10 50)) "%).")
-      ", no data about PM 10 in this location.")))
-
-(defn get-final-str [city]
-  (let [air-quality-info (waqi/get-air-quality city)]
-    (if (location-str air-quality-info)
-      (str (location-str air-quality-info) (aqi-str air-quality-info) (pm25-str air-quality-info) (pm10-str air-quality-info))
-      "Location not found")))
 
 (defn -main
   [city]
-  (println (get-final-str city)))
+  (println (ui/get-final-str city)))
