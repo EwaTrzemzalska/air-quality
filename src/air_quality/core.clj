0f85f2584922b465df686d0164c7ce6d68dcd483
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-20 17:29:38 +0100

    Refactoring

--- src/air_quality/core.clj
+++ src/air_quality/core.clj
@@ -1,7 +1,12 @@
 (ns air-quality.core
-  (:require [air-quality.ui :as ui])
+  (:require [air-quality.ui :as ui]
+            [air-quality.waqi :as waqi])
   (:gen-class))
 
 (defn -main
   [city]
-  (println (ui/build-result-str city)))
+  
+  (-> city
+      (waqi/get-air-quality)
+      (ui/build-result-str)
+      (println)))
