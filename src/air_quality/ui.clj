35e334580b4f07f200f9205fc0ffa7ff40708777
Ewa Trzemżalska <trzemson@gmail.com>
2020-01-20 17:51:20 +0100

    Refactoring nested if's for if-let and when-let

--- src/air_quality/ui.clj
+++ src/air_quality/ui.clj
@@ -13,31 +13,27 @@
   (get-in air-quality-info [:data :iaqi :pm10 :v]))
 
 (defn- build-location-str [air-quality-info]
-  (let [location (air-quality-info->location air-quality-info)]
-    (when location
-      (str "In " location))))
+  (when-let [location (air-quality-info->location air-quality-info)]
+    (str "In " location)))
 
 (defonce aqi-norm 50)
 (defonce pm25-norm 25)
 (defonce pm10-norm 50)
 
 (defn- build-aqi-str [air-quality-info]
-  (let [aqi (air-quality-info->aqi air-quality-info)]
-    (if aqi
-      (str " - AQI: " aqi " (" (* 100 (/ aqi aqi-norm)) "%)")
-      "no data about AQI, ")))
+  (if-let [aqi (air-quality-info->aqi air-quality-info)]
+    (str " - AQI: " aqi " (" (* 100 (/ aqi aqi-norm)) "%)")
+    "no data about AQI, "))
 
 (defn- build-pm25-str [air-quality-info]
-  (let [pm25 (air-quality-info->pm25 air-quality-info)]
-    (if pm25
-      (str ", PM 2.5: " pm25 " μg/m3 (" (* 100 (/ pm25 pm25-norm)) "%)")
-      ", no data about PM 2.5 in this location")))
+   (if-let [pm25 (air-quality-info->pm25 air-quality-info)]
+     (str ", PM 2.5: " pm25 " μg/m3 (" (* 100 (/ pm25 pm25-norm)) "%)")
+     ", no data about PM 2.5 in this location"))
 
 (defn- build-pm10-str [air-quality-info]
-  (let [pm10 (air-quality-info->pm10 air-quality-info)]
-    (if pm10
-      (str ", PM 10: " pm10 " μg/m3 (" (* 100 (/ pm10 pm10-norm)) "%).")
-      ", no data about PM 10 in this location.")))
+  (if-let [pm10 (air-quality-info->pm10 air-quality-info)]
+    (str ", PM 10: " pm10 " μg/m3 (" (* 100 (/ pm10 pm10-norm)) "%).")
+    ", no data about PM 10 in this location."))
 
 (defn build-result-str [air-quality-info]
     (if-let [location-str (build-location-str air-quality-info)]
